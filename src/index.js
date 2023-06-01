5294f6ce5c55c78cdf71d3a7ef41cfcd93c34e5b
MokoneSA <simonlephotojr@gmail.com>
2023-06-01 14:18:42 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import { render } from '@testing-library/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root'));
-render(<App />);
+ReactDOM.createRoot((document.getElementById('root'))).render(<App />);
